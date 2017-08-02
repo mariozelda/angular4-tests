7f763f7fb7c1475b3977b3705e528fbea336d1c7
Mario Celda <mario.celda@inpeek.ch>
2017-08-02 11:01:52 +0200

    Dropdown Header menus with correct styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
-import { Component } from '@angular/core';
+import {Component, NgModule, VERSION} from '@angular/core'
+import {BrowserModule} from '@angular/platform-browser'
 import { FFDAOMeterReadingService } from './flex-frame/core/infrastructure/services/ff-dao-meter-reading.service';
+
+import { MdButtonModule, MdCardModule, MdIconModule, MdMenuModule, MdToolbarModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/common/header/header.component';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +12,27 @@ import { FFDAOMeterReadingService } from './flex-frame/core/infrastructure/servi
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  menuItems = [
+    {
+      text: 'John Doe',
+      items: [
+        {
+          text: 'Profile'
+        },
+        {
+          text: 'Settings'
+        },
+        {
+          text: 'Account'
+        },
+        {
+          text: 'Sign Out'
+        }
+      ]
+    }
+  ];
+
   constructor(private meterReadingService: FFDAOMeterReadingService) {
     this.meterReadingService.getMeterReadingResultSet('169013').subscribe(
       (data) => {
@@ -16,3 +42,18 @@ export class AppComponent {
   }
   title = 'app';
 }
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MdToolbarModule,
+    MdMenuModule,
+    MdButtonModule,
+    MdIconModule,
+    MdCardModule
+  ],
+  declarations: [ AppComponent, HeaderComponent],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
--- src/app/components/common/header/header.component.ts
+++ src/app/components/common/header/header.component.ts
@@ -1,21 +1,58 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input, QueryList, ViewChildren} from '@angular/core';
 import { Router } from '@angular/router';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { ConnectedPositionStrategy, MdMenuTrigger } from '@angular/material';
+
+let originWithFallbackPosition = ConnectedPositionStrategy.prototype.withFallbackPosition;
+ConnectedPositionStrategy.prototype.withFallbackPosition = function (originPos, overlayPos) {
+  overlayPos.overlayX = 'start';
+  if(this._preferredPositions.length) {
+    return this;
+  }
+  return originWithFallbackPosition.apply(this, arguments);
+};
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
-  // ,
-  // template: '<app-navbar (click)="mainNav.toggleSidenav()"></app-navbar>',
-
 })
 export class HeaderComponent implements OnInit {
+
+  // @Input() items: any[];
+
+  @ViewChildren(MdMenuTrigger) triggers: QueryList<MdMenuTrigger>;
 
   @Output() navToggle = new EventEmitter<boolean>();
   isOpened = false;
 
-  constructor( private router:Router) {
+  menuItems:any = [
+    {
+      items: [
+        {
+          text: 'Profile'
+        },
+        {
+          text: 'Settings'
+        },
+        {
+          text: 'Account'
+        },
+        {
+          text: 'Sign Out'
+        }
+      ]
+    }
+  ];
 
+  languages = [
+    { text:'DE' },
+    { text:'EN' },
+    { text:'FR' }
+  ];
+
+  defaultLanguage = this.languages[0];
+
+  constructor( private router:Router) {
   }
 
   ngOnInit() {
@@ -29,5 +66,20 @@ export class HeaderComponent implements OnInit {
     this.navToggle.emit(true);
   }
 
+  openMenu(trigger: MdMenuTrigger, level: number) {
+    this.triggers
+      .filter((x: any) => x._element.nativeElement.dataset.level >= level && x !== trigger)
+      .forEach(x => x.closeMenu());
+    trigger.openMenu();
+  }
+
+  closeMenu() {
+    this.triggers.forEach(x => x.closeMenu());
+  }
+
+  changeLanguage() {
+      // console.log("Change language!");
+      this.triggers.forEach(x => x.closeMenu());
+  }
 
 }
